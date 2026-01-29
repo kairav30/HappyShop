537f67dbdb0e691c05338b81b816ef2d1073ac6f
kairav <K.nayee2@uni.brighton.ac.uk>
2026-01-29 22:36:19 +0000

    created Guest button in LoginView.java

--- src/main/java/ci553/happyshop/client/login/LoginView.java
+++ src/main/java/ci553/happyshop/client/login/LoginView.java
@@ -1,4 +1,15 @@
 package ci553.happyshop.client.login;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import javafx.stage.Stage;
 public class LoginView {
+    public LoginController loginController;
+
+    public void start(Stage stage){
+        Label Title = new Label("Welcome to Kairavs happy shop!");
+
+        Button Btn_Guest = new Button("Continue as a Guest");
+    }
 }
