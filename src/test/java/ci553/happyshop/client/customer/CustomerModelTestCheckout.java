c319f9e38d2f7df100968a794eea0f821dd0a6aa
kairav <K.nayee2@uni.brighton.ac.uk>
2026-01-30 12:12:55 +0000

    checkout() test file

---
+++ src/test/java/ci553/happyshop/client/customer/CustomerModelTestCheckout.java
@@ -0,0 +1,12 @@
+package ci553.happyshop.client.customer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CustomerModelTestCheckout {
+
+    @Test
+    void checkOut() {
+    }
+}
