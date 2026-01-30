5e3d05543d77e9606321a53ba6b15dae2a04c721
kairav <K.nayee2@uni.brighton.ac.uk>
2026-01-30 00:25:34 +0000

    created method for guest button

--- src/main/java/ci553/happyshop/client/login/LoginController.java
+++ src/main/java/ci553/happyshop/client/login/LoginController.java
@@ -1,4 +1,53 @@
 package ci553.happyshop.client.login;
+import ci553.happyshop.client.customer.*;
+import ci553.happyshop.client.emergency.EmergencyExit;
+import ci553.happyshop.client.orderTracker.OrderTracker;
+import ci553.happyshop.client.picker.PickerController;
+import ci553.happyshop.client.picker.PickerModel;
+import ci553.happyshop.client.picker.PickerView;
+import ci553.happyshop.client.warehouse.*;
+import ci553.happyshop.orderManagement.OrderHub;
+import ci553.happyshop.storageAccess.DatabaseRW;
+import ci553.happyshop.storageAccess.DatabaseRWFactory;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.*;
+import java.util.List;
 
 public class LoginController {
+
+    public  LoginView loginView;
+    public void Guests(Stage stage){
+        CustomerView view = new CustomerView();
+        CustomerController controller = new CustomerController();
+        CustomerModel model = new CustomerModel();
+        DatabaseRW databaseRW = DatabaseRWFactory.createDatabaseRW();
+
+        view.cusController = controller;
+        controller.cusModel = model;
+        model.cusView = view;
+        model.databaseRW = databaseRW;
+
+        view.start(stage);
+        PickerModel pickerModel = new PickerModel();
+        PickerView pickerView = new PickerView();
+        PickerController pickerController = new PickerController();
+        pickerView.pickerController = pickerController;
+        pickerController.pickerModel = pickerModel;
+        pickerModel.pickerView = pickerView;
+        pickerModel.registerWithOrderHub();
+        pickerView.start(new Stage());
+        OrderTracker orderTracker = new OrderTracker();
+        orderTracker.registerWithOrderHub();
+        OrderHub.getOrderHub().initializeOrderMap();
+
+
+    }
+
 }
