1af95a1673d789647fbb86836aa89aebc3edd1c4
kairav <K.nayee2@uni.brighton.ac.uk>
2026-01-30 11:46:32 +0000

    CustomerModel Class test file

---
+++ src/test/java/ci553/happyshop/client/customer/CustomerModelTest.java
@@ -0,0 +1,19 @@
+package ci553.happyshop.client.customer;
+
+import javafx.application.Platform;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class CustomerModelTest {
+    @BeforeAll
+    static void initJavaFX() {
+        Platform.startup(() -> {});
+    }
+    @Test
+    void addToTrolley() {
+
+
+
+    }
+}
